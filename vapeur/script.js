7e2feef8aa9d138b62989ae09a17b1fc5869ad97
selim <selimb30@outlook.fr>
2025-12-09 08:55:14 +0100

    first commit
    first commit

---
+++ vapeur/script.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+const bodyParser = require("body-parser")
+const path = require('path');
+const hbs = require("hbs");
+
+const app = express();
+const port = 8000;
+
+app.set("view engine", "hbs"); // On définit le moteur de template que Express va utiliser
+app.set("views", path.join(__dirname, "views")); // On définit le dossier des vues (dans lequel se trouvent les fichiers .hbs)
+hbs.registerPartials(path.join(__dirname, "views", "partials"));
+app.use(bodyParser.urlencoded())
+
+hbs.registerHelper("formatDate", (date) => {
+    return date.toLocaleDateString();
+});
+
+app.get("/", async (req, res) => {
+    // on passe seulement le nom du fichier .hbs sans l'extention.
+    // Le chemin est relatif au dossier views.
+    // On peut aller chercher des templates dans les sous-dossiers (e.g. movies/details).
+    console.log("bonjour");
+});
