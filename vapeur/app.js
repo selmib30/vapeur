e83aa1235e79eeae9731bd6a227981e18d664026
selim <selimb30@outlook.fr>
2025-12-15 13:56:29 +0100

    genreseeder
    mise en place du script

--- vapeur/app.js
+++ vapeur/app.js
@@ -4,9 +4,9 @@ const prisma = new PrismaClient();
 const bodyParser = require("body-parser")
 const path = require('path');
 const hbs = require("hbs");
-
 const app = express();
 const port = 8000;
+const { seedGenres } = require('./GenreSeeder');
 
 app.set("view engine", "hbs"); // On définit le moteur de template que Express va utiliser
 app.set("views", path.join(__dirname, "views")); // On définit le dossier des vues (dans lequel se trouvent les fichiers .hbs)
@@ -16,6 +16,14 @@ app.use(bodyParser.urlencoded())
 hbs.registerHelper("formatDate", (date) => {
     return date.toLocaleDateString();
 });
+
+(async () => {
+  await seedGenres(prisma);
+  
+  app.listen(port, () => {
+    console.log(`Serveur démarré sur http://localhost:${port}`);
+  });
+})();
 
 app.get("/", async (req, res) => {
     // on passe seulement le nom du fichier .hbs sans l'extention.
