685a5a094a1cb1b26a493b54ff541a760fc259b9
papynoel <clementbreuil123@gmail.com>
2025-12-15 11:01:57 +0100

    hbs

--- vapeur/app.js
+++ vapeur/app.js
@@ -23,3 +23,16 @@ app.get("/", async (req, res) => {
     // On peut aller chercher des templates dans les sous-dossiers (e.g. movies/details).
     console.log("bonjour");
 });
+
+app.get("/", async (req, res) => {
+    // on passe seulement le nom du fichier .hbs sans l'extention.
+    // Le chemin est relatif au dossier `views`.
+    // On peut aller chercher des templates dans les sous-dossiers (e.g. `movies/details`).
+    res.render("index");
+});
+const hbs = require("hbs");
+
+// Configuration de Handlebars pour Express
+app.set("view engine", "hbs"); // On définit le moteur de template que Express va utiliser
+app.set("views", path.join(__dirname, "views")); // On définit le dossier des vues (dans lequel se trouvent les fichiers .hbs)
+hbs.registerPartials(path.join(__dirname, "views", "partials")); // On définit le dossier des partials (composants e.g. header, footer, menu...)
